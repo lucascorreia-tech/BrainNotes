e21a75d2e51dcb22dab608d3f1032e6859e80044
Lucas Correia <164992636+lucascorreia-tech@users.noreply.github.com>
2026-03-12 17:57:51 -0300

    Fix base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lucascorreia-tech.github.io',
-  base: 'BrainNotes',
+  base: '/BrainNotes',
   vite: {
     plugins: [tailwindcss()]
   }
