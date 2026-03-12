6b76453407153c4033241f2ef526ee0b9a66c4fe
lucascorreia-tech <lucasrcorreia23@gmail.com>
2026-03-12 17:51:05 -0300

    "Configuração do deploy"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lucascorreia-tech.github.io',
+  base: 'BrainNotes',
   vite: {
     plugins: [tailwindcss()]
   }
