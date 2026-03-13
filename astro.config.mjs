e63c4406ea5eff3d1f9ff235f30dfcffac23c7c2
lucascorreia-tech <lucasrcorreia23@gmail.com>
2026-03-13 10:50:27 -0300

    "Ajeitando a indentação do código"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lucascorreia-tech.github.io',
-  base: 'BrainNotes',
+  site: "https://lucascorreia-tech.github.io",
+  base: "BrainNotes",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
